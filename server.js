05f3387b5bcb7a2ecb4605c05be245f4c950c86f
Deepakdhakad075 <ddhakad483@gmail.com>
2025-10-15 19:57:03 +0530

    add frontend live url in cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000', // Local frontend
   'https://zoomcarride.onrender.com', // Replace with your deployed frontend
-  'https://carrental-frontend-rguy.onrender.com', // Example Render frontend URL
+  'https://zoomridecarss.onrender.com', // Example Render frontend URL
 ];
 
 app.use(
